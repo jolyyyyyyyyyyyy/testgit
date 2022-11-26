36c44046655c76ed0a9519ff4401f4c0910a5f1f
jolyyyyyyyyyyyy <amaliamironenko2804@mail.ru>
2022-11-26 21:45:16 +0300

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    Function1()
+    Function2()
+}
+fun Function1() {
+    print("подсчитывает количество знаков пунктуации в строке\n")
+    val symbols = "java, kotlin, python, php".count({ letter ->
+        letter == ','
+    })
+    print("Ответ: $symbols знаков пунктуации в строке")
+    println()
+}
+fun Function2() {
+    print("подсчитывает количество цифр в строке\n")
+    val text = "в 2023 году этот ноутбук будет стоит 150к"
+    var digits = 0
+    for (i in 0..text.length - 1) {
+        val num = text[i]
+        if (num in '0'..'9') {
+            digits++
+        }
+    }
+    println("Ответ: $digits цифр в строке")
 }
